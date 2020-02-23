5982778a5bc3e77da52517e9f7fecf7b42899e07
Alex Zafiris <zafiris.alex@yahoo.com>
2020-02-23 15:14:03 +0200

    Update pyqt test.py

--- pyqt test.py
+++ pyqt test.py
@@ -6,11 +6,11 @@ import requests
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import (QApplication, QComboBox, QSpinBox, QInputDialog, QLabel, QLineEdit,
+from PyQt5.QtWidgets import (QApplication, QComboBox, QListWidget, QLabel, QLineEdit,
                              QMainWindow, QPlainTextEdit, QPushButton, QWidget)
 
 username = getpass.getuser()
-
+    
 class App(QMainWindow):
     
     def __init__(self):
@@ -57,11 +57,11 @@ class App(QMainWindow):
         
         city = str(self.city_name.currentText())
         
-        self.num_days = QSpinBox(self)
+        self.num_days = QLineEdit(self)
         self.num_days.resize(120 , 25)
         self.num_days.move(350, 210)
         
-        days = str(self.num_days.value())
+        days = self.num_days.text()
 
         self.button1 = QPushButton('Get Forecast', self)
         self.button1.setFont(QFont("Verdana", 10, QFont.Bold))
@@ -82,33 +82,49 @@ class App(QMainWindow):
             }
 
             response = requests.request("GET", url, headers=headers, params=querystring)
-            data = response.json()
-            data2 = response.text
-            for day in data['list']:
-                date = str(datetime.datetime.fromtimestamp(day['dt']).strftime('%d-%m-%y'))
-                weather =str(day['weather'][0]['description'])
-                min_temp = float(day['temp']['min'])
-                max_temp = float(day['temp']['max'])
-                hum = int(day['humidity'])
-        
+            self.data = response.json()
+            self.data2 = response.text
+            print(self.data)
+            
+                        
         def open_forecast():
-                
+            self.msg = QListWidget()
+            self.msg.move(270, 370)
+            self.msg.insertItem(0, "Forecast for " + city + '\n')
+            for day in self.data['list']:
+                self.date = str(datetime.datetime.fromtimestamp(day['dt']).strftime('%d-%m-%y'))
+                self.weather =str(day['weather'][0]['description'])
+                self.min_temp = float(day['temp']['min'])
+                self.max_temp = float(day['temp']['max'])
+                self.hum = int(day['humidity'])
+                self.text = 'DATE:'
+                self.text2 = 'Weather:'
+                self.text3 = 'Temp min:'
+                self.text4 = 'Temp max:'
+                self.text5 = 'Humidity:'
+                self.d = '{0} {1}'.format(self.text, self.date)
+                self.w = '{0} {1}'.format(self.text2, self.weather)
+                self.mi = '{0} {1}'.format(self.text3, self.min_temp)
+                self.ma = '{0} {1}'.format(self.text4, self.max_temp)
+                self.h = '{0} {1}{2}'.format(self.text5, self.hum, '%')
+                self.msg.insertItem(1, self.d + "\n" + self.w + '\n' + self.mi + "\n" + self.ma + "\n" + self.h + '\n')
+            self.msg.show()
         
         self.button1.clicked.connect(get_forecast)
         
         self.button2 = QPushButton('Show Forecast', self)
-        self.button2.clicked.connect(open_forecast)
         self.button2.setFont(QtGui.QFont("Verdana", 10, QFont.Bold))
         self.button2.setStyleSheet('QPushButton {background-color: blue; color: white;}')
         self.button2.resize(120, 30)
-        self.button2.move(290, 365) 
+        self.button2.move(290, 330)
+        self.button2.clicked.connect(open_forecast) 
         
         self.button3 = QPushButton('Exit', self)
         self.button3.clicked.connect(QCoreApplication.instance().quit)
         self.button3.setFont(QtGui.QFont("Verdana", 10, QFont.Bold))
         self.button3.setStyleSheet('QPushButton {background-color: black; color: white;}')
         self.button3.resize(80, 30)
-        self.button3.move(310, 450) 
+        self.button3.move(310, 460) 
         self.show()
         
 
